f5df82a32319780a9694f2717232ff64bf8ad23a
Glitch (watcheye-node) <none>
2020-08-10 21:40:50 +0000

    💋🙋 Updated with Glitch

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("*", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(function(err, req, res, next) { //This will or may help with Failed to decode problem.
+    res.redirect('/views/index.html');
+    next(err); 
+  });
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
